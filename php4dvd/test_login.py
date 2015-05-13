fb16190760ccab3fea44ad4a5499c10d95cfde42
stakejimmy <stakejimmy@gmail.com>
2015-05-13 09:16:38 +0300

    added log methods

--- php4dvd/test_login.py
+++ php4dvd/test_login.py
@@ -3,13 +3,18 @@ from selenium import webdriver
 from selenium.common.exceptions import *
 from selenium_fixture import driver
 
+def logout(driver):
+    driver.find_element_by_link_text("Log out").click()
+    driver.switch_to_alert().accept()
 
 def test_login(driver):
-    driver.get("http://localhost/php4dvd/")
     driver.find_element_by_id("username").clear()
     driver.find_element_by_id("username").send_keys("admin")
     driver.find_element_by_name("password").clear()
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("submit").click()
-    driver.find_element_by_link_text("Log out").click()
-    driver.switch_to_alert().accept()
+
+def test_login(driver):
+    driver.get("http://localhost/php4dvd/")
+    login(driver)
+    logout(driver)
